1b5e867ee495a71826f3c606b9140d30626b4391
Rohan Singh <rohan.b.singh54@gmail.com>
2022-12-02 10:16:32 -0500

    Created assignment file for Week 1 lab

---
+++ Week 1/helloworld.c
@@ -0,0 +1,21 @@
+//Author: Rohan Singh
+
+#include <stdio.h>
+
+int main(){
+
+	//Uncomment the working print statement(s)
+	//System.out.println("Hello World!\n");
+	//print("Hello World!\n");
+	//printf("Hello World!\n");
+	//fprintf(stdout,"Hello World!\n");
+	
+	//Make a print statement for your name!!!
+
+	//Make a print statement that prints out: "I am *your age* years old!"
+	int age;	//Initilize the variable
+	//Put the print statement here
+
+	return 0;
+
+}
