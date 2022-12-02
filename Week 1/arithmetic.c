5a500fd3edb743b19cb54500fb4b8ef9ad6585f0
Rohan Singh <rohan.b.singh54@gmail.com>
2022-12-02 10:42:48 -0500

    Added file for arithmetic operations

---
+++ Week 1/arithmetic.c
@@ -0,0 +1,42 @@
+//Author: Rohan Singh
+
+#include <stdio.h>
+
+int main(){
+	
+	//These are the ints that you will use for the arithmetic operations	
+	int x = 18;
+	int y = 11;
+
+	//Store your results in this int
+	int result;
+
+
+	//Do addition here:
+
+	//Print result:
+
+
+	//Do subtraction here:
+
+        //Print result:
+
+
+        //Do multiplication here:
+
+        //Print result:
+
+
+        //Do division here:
+
+        //Print result:
+
+
+        //Do modulus here:
+
+        //Print result:
+
+       	
+	return 0;
+	
+}
