ba820d47f06ef35fdb6cfb64eb8cce9b953e825b
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-12-04 18:16:20 -0500

    Created script for variables

---
+++ Week 4/vargreeting.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+# In bash the variables and assignments shouldn't have any spaces between them
+greeting=Hello
+forename=Alessandra
+lastname=Puccio
+
+echo $greeting $forename $lastname!
