83158b90b9cd4e021e3d39b1aabfeba243e2af85
Rohan Singh <rohan.b.singh54@gmail.com>
2022-12-08 20:30:54 -0500

    Added multithreading code to Week 10 module

---
+++ Week 10/threading.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int *count;
+
+int incr();
+void *mythreadstart();
+
+int main(){
+	pthread_t t1, t2;
+	count = malloc(sizeof(int));
+	pthread_create(&t1,NULL,mythreadstart,NULL);
+	pthread_create(&t2,NULL,mythreadstart,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	fprintf(stdout,"The value of count is: %d\n",*count);
+	return 0;
+}
+
+void *mythreadstart(){
+	for(int i = 0; i < 1000000; i++)
+		incr();
+	return NULL;
+}
+
+int incr(){
+	(*count)++;
+	return 1;
+}
+
+
