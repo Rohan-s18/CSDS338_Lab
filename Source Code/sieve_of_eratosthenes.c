b71857bc004dacd813a1c167cf3e5bc8be748fa4
Rohan Singh <rohan.b.singh54@gmail.com>
2022-12-22 15:48:19 -0800

    Added Comments

--- Source Code/sieve_of_eratosthenes.c
+++ Source Code/sieve_of_eratosthenes.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
-#import <stdlib.h>
-#import <string.h>
+#include <stdlib.h>
+#include <string.h>
 
+//This global variable will be used to store the number of prime numbers that we need
 int nprimes;
+
+//This pointer will be used to store all of the 'nprimes' prime numbers
 int *prime;
 
+/*
+int argc: This tells us how many command line arguments are passed, the intial value is 1 (name of process)
+char *argv[]: This is the array of strings containing the command line arguments
+*/
 int main(int argc, char *argv[]) {
-	int i;
-	int current = 2;
+
+	//Getting the number of primes from the command line arguments
 	nprimes = atoi(argv[1]);
+
+	/* 
+	Intiliazing the array:
+	malloc(nprimes*sizeof(int)): This will allocate nprimes*(sizeof(int)) amount of memory to the process
+	(int *) : This means that the allocated memory will be used as a pointer to ints, which you may by this point know means it is an array of ints
+	*/
 	prime = (int *)malloc(nprimes*sizeof(int)); 
+
+	int current = 2;
 	prime[0] = current;
-	for (i=1; i<nprimes; i++) {
-		int j; 
+
+	//This segment of code is not really useful for the purposes of the course 
+	for (int i=1; i<nprimes; i++) {
 		NewCandidate:
 			current++;
-			for (j=0; prime[j]*prime[j] <= current; j++){
-				if (current % prime[j] == 0) goto NewCandidate;
+			for (int j=0; prime[j]*prime[j] <= current; j++){
+				if (current % prime[j] == 0) 
+					goto NewCandidate;
 			}
-      			prime[i] = current;
+      		prime[i] = current;
    	}
 	for(int k = 0; k < nprimes; k++)
 		fprintf(stdout,"Prime number %d is: %d\n",k+1,prime[k]);
