51b9e8625622e9b768c613f1cab9366aafd2ed59
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-03 09:26:59 -0800

    Added file for demonstration of void pointers in C

---
+++ Source Code/Pointers/void_pointer.c
@@ -0,0 +1,16 @@
+//  Author: Rohan Singh
+//  1/3/2023
+//  This file will demonstrate how to use "void pointers" in C
+
+//  Imports
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//  Main method to demonstrate the usage of void pointers
+int main(){
+
+
+    return 0;
+}
