65db59452def783033e3186ddd1be19d6a7ac7ff
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-15 23:44:48 -0500

    Added Code for Emitter

---
+++ Compiler/emit.py
@@ -0,0 +1,19 @@
+class Emitter:
+    def __init__(self, fullPath):
+        self.fullPath = fullPath
+        self.header = ""
+        self.code = ""
+
+    def emit(self, code):
+        self.code += code
+
+    def emitLine(self, code):
+        self.code += code + '\n'
+
+    def headerLine(self, code):
+        self.header += code + '\n'
+
+    def writeFile(self):
+        with open(self.fullPath, 'w') as outputFile:
+            outputFile.write(self.header + self.code)
+
