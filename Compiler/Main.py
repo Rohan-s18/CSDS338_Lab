fc13b6d86dc5ab77249f44c2dfb89f976c1272c1
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-15 23:46:51 -0500

    Added Main Runner module

---
+++ Compiler/Main.py
@@ -0,0 +1,26 @@
+
+
+from lex import *
+from emit import *
+from parse import *
+import sys
+
+def main():
+    print("CSDS 338 Compiler!")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        input = inputFile.read()
+
+    # Initialize the lexer, emitter, and parser.
+    lexer = Lexer(input)
+    emitter = Emitter("out.c")
+    parser = Parser(lexer, emitter)
+
+    parser.program() # Start the parser.
+    emitter.writeFile() # Write the output to file.
+    print("Compiling completed.")
+
+if __name__ == "__main__":
+    main()
